3ebe5583fbe51d36fa640ec218fdc6f54ed3e8bf
Jordan Holliday <J-Holliday@outlook.com>
2018-10-08 04:00:02 -0500

    Updated Game's Score method to pass all unit tests.
    Added functionality to check for spares and add the following rolls score as a bonus in the case of them.
    
    Fixed a failing unit test in the check for Strikes which was counting scoring a 0 on the first roll and a 10 on the second roll as a strike which would calculate incorrectly, now makes sure it is the first throw of the round before counting as a strike.
    
    Cleaned up and finalised code in the Game class.

--- ScoringBowlingKata/Game.cs
+++ ScoringBowlingKata/Game.cs
@@ -2,6 +2,10 @@
 
 namespace ScoringBowlingKata
 {
+    /// <summary>
+    /// Class for managing a scoring card for a game of bowling, contains methods for recording pins knocked down and 
+    /// calculating a final score.
+    /// </summary>
     public class Game
     {
         private List<int> Rolls = new List<int>();
@@ -15,6 +19,10 @@ namespace ScoringBowlingKata
             Rolls.Add(pins);
         }
 
+        /// <summary>
+        /// Calculation method for returning the final score of the game.
+        /// </summary>
+        /// <returns>Integer for the total end game score value.</returns>
         public int Score()
         {
             int total = 0;
@@ -25,9 +33,15 @@ namespace ScoringBowlingKata
             {
 
                 // Check for and add strike bonus to the total.
-                if ((i + 2 < Rolls.Count) && (Rolls[i] == 10))
+                if ((round % 1 == 0) && (i + 2 < Rolls.Count) && (Rolls[i] == 10))
                 {
                     total = total + Rolls[i + 1] + Rolls[i + 2];
+                }
+                
+                // Check for and add spare bonus to the total.
+                else if ((round % 1 != 0) && (i + 1 < Rolls.Count) && (Rolls[i] + Rolls[i - 1] == 10))
+                {
+                    total = total + Rolls[i + 1];
                 }
 
                 // Add the roll value to total if not a bonus only roll.
