2c38b21c55080b2326688eb808369cb91edae6ca
Jordan Holliday <J-Holliday@outlook.com>
2018-10-08 03:00:03 -0500

    Added NUnit framework to solution. Created a new skeleton test class to test the games score calculation method.

---
+++ ScoringBowlingKata/GameTests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace ScoringBowlingKata
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        [Test]
+        public void TestScore()
+        {
+            Assert.Fail();
+        }
+    }
+}
