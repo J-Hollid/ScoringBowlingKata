449ea18d8a13f8eb2835d650ba1e7d9c6d9a4e89
Jordan Holliday <J-Holliday@outlook.com>
2018-10-08 03:10:29 -0500

    Added a new private List of integer values to record the number of pins knocked down each roll.
    Added functionality to the Roll method in order to record each roll to the newly created list.

--- ScoringBowlingKata/Game.cs
+++ ScoringBowlingKata/Game.cs
@@ -1,10 +1,18 @@
-﻿namespace ScoringBowlingKata
+﻿using System.Collections.Generic;
+
+namespace ScoringBowlingKata
 {
     public class Game
     {
+        private List<int> Rolls = new List<int>();
+
+        /// <summary>
+        /// Input method for recording number of pins knocked down.
+        /// </summary>
+        /// <param name="pins">Integer value indicating the number of pins knocked down.</param>
         public void Roll(int pins)
         {
-
+            Rolls.Add(pins);
         }
 
         public int Score()
