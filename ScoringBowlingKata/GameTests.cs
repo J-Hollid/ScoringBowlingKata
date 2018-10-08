d386357507ac2866f1e397e132e296303082411e
Jordan Holliday <J-Holliday@outlook.com>
2018-10-08 03:05:19 -0500

    Added first failing TestCase to test a simple game where no strikes or spares are scored

--- ScoringBowlingKata/GameTests.cs
+++ ScoringBowlingKata/GameTests.cs
@@ -5,22 +5,25 @@ namespace ScoringBowlingKata
     [TestFixture]
     public class GameTests
     {
+        private Game TestGame;
+
         [SetUp]
         public void SetUp()
         {
-            
+            TestGame = new Game();  
         }
 
-        [TearDown]
-        public void TearDown()
+        [TestCase(new int[20] { 3, 3, 3, 0, 0, 0, 0, 0, 2, 5, 6, 2, 2, 5, 5, 3, 3, 5, 3, 3 }, ExpectedResult = 53, Description = "Test no strikes/spares.")]
+        public int TestScore(int[] rolls)
         {
-            
-        }
+            // Input roles.
+            foreach (int roll in rolls)
+            {
+                TestGame.Roll(roll);
+            }
 
-        [Test]
-        public void TestScore()
-        {
-            Assert.Fail();
+            // Return actual result for comparison against the expected.
+            return TestGame.Score();
         }
     }
 }
